ea7dae626eea215d86ab2becbcfe46bf7a5175aa
Martin Trnka <75213892+Trnass@users.noreply.github.com>
2023-12-12 06:52:50 +0100

    opraven endpoint a přidáno hledání podle ID

--- main.py
+++ main.py
@@ -2,27 +2,26 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import datetime
 
-username = "jmeno" # Uživatelské jméno
-password = "heslo" # Heslo
-
 date = datetime.datetime.now()
+
+username = "jmeno"
+password = "heslo"
 
 if date.month == 12 and 1 <= date.day <= 24:
     print("Datum je v rozmezí adventního kalendáře.")
 
     chrome_options = Options()
     chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox") 
+    chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
     driver = webdriver.Chrome(executable_path='/etc/drivers/chromedriver', options=chrome_options)
 
-    login_url = 'https://leosight.cz/prihlaseni'
-    driver.get(login_url)
+    driver.get('https://leosight.cz/')
 
-    driver.find_element_by_name('username').send_keys(username)
-    driver.find_element_by_name('password').send_keys(password)
-    driver.find_element_by_name('submit').click()
+    driver.find_element_by_id('login-username').send_keys(username)
+    driver.find_element_by_id('login-password').send_keys(password)
+    driver.find_element_by_id('up-login').click()
 
     advent_url = 'https://leosight.cz/advent'
     driver.get(advent_url)
